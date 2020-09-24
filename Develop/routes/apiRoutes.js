e6119df7ec224bddb90d9245ca13d7d929f6e880
Jared Varnum <jvarnum3@gmail.com>
2020-09-23 22:34:56 -0500

    added server.js, working on routes, npm installed packages, added .gitignore file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,63 +1,33 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+//REQUIRE
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+const db = require("../db/db.json");
+var fs = require("fs");
 
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+//=============================================================================
+//ROUTING
+//=============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+  //API GET request
+  app.get("/api/notes", function(req, res) {
+    res.json(db);
+  })
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
-  });
+//Post request
+app.post("/api/notes", function(req,res) {
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
+    //within this function allow save to request body, and THEN add it to db.json file
+    db.push(req.body);
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+    //add unique identifier to each note (so you can select individually if needed)
+    //forEach loop
+    db.forEach((obj, i) => {
+      obj.id = i + 1;
+    })
 
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
+    //now return the new note to the client
+    fs.writeFile("./db/db.json", JSON.stringify(db), function(){
+      res.json(db);
+    });
   });
 };
---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+
+
+// Sets the port
+var PORT = process.env.PORT || 3000;
+
+// Sets the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+//routes from server to user
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
