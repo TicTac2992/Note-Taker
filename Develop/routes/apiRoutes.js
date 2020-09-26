8c78d135278fa1b9a0d4a8c4a7d42c8dc33741e5
Jared Varnum <jvarnum3@gmail.com>
2020-09-26 00:50:03 -0500

    fixed closing bracket in route html

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -8,14 +8,13 @@ var fs = require("fs");
 //=============================================================================
 
 module.exports = function(app) {
-    //API GET request
-    app.get("/api/notes", function(req, res) {
-      res.json(db);
-    })
+  //API GET request
+  app.get("/api/notes", function(req, res) {
+    res.json(db);
+  });
 
-//Post request
-app.post("/api/notes", function(req,res) {
-
+  //Post request
+  app.post("/api/notes", function(req,res) {
     //within this function allow save to request body, and THEN add it to db.json file
     db.push(req.body);
 
@@ -26,26 +25,26 @@ app.post("/api/notes", function(req,res) {
     })
 
     //now return the new note to the client
-    fs.writeFile("./db/db.json", JSON.stringify(db), function(){
+    fs.writeFile("./db/db.json", JSON.stringify(db), function() {
       res.json(db);
-    });
+    })
   });
-};
+    //API delete request
+    app.delete("/api/notes/:id", function(req, res) {
+          var id = req.params.id;
 
-//API delete request
- app.delete("/api/notes/:id", function(req, res) {
-  var id = req.params.id;
+    //splice deletes the selected note from the db array
+    db.splice(id - 1, 1);
 
-  //splice deletes the selected note from the db array
-  db.splice(id - 1, 1);
-
-  //reassign id for each note object
-  db.forEach((obj, i) => {
-      obj.id = i + 1;
-  });
+        //reassign id for each note object
+        db.forEach((obj, i) => {
+          obj.id = i + 1;
+        });
+      
 
   //return remaining notes to client
-  fs.writeFile("./db/db.json", JSON.stringify(db), function (){
-      res.json(db);
-  });
-});
+      fs.writeFile("./db/db.json", JSON.stringify(db), function (){
+          res.json(db);
+      });
+    });
+  };
